7a85f94955b722dd5317a42c396424828e0d210b
Grimmer Kang <grimmer0125@gmail.com>
2016-09-21 03:24:33 +0800

    modify script.

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+# echo "try to pull image"
+# docker pull grimmer0125/openface-nostartdemo
+# echo "after pulling"
 docker stop faceserver
 docker rm faceserver
+echo "try to docker run"
 docker run -d -p 9000:9000 -v $(pwd)/electricface/:/demo -t -i --name faceserver grimmer0125/openface-nostartdemo bash -c "/demo/src/run.sh"
