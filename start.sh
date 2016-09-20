c70af04fc26f1d5e862a6ea0e1d675d205e320d1
Grimmer Kang <grimmer0125@gmail.com>
2016-09-20 21:42:38 +0800

    add scripts to launch python server in docker.

---
+++ start.sh
@@ -0,0 +1,3 @@
+docker stop faceserver
+docker rm faceserver
+docker run -d -p 9000:9000 -v $(pwd)/electricface/:/demo -t -i --name faceserver grimmer0125/openface-nostartdemo bash -c "/demo/src/run.sh"
