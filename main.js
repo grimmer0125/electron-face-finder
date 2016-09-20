8292c2a6213a0fd25e11af3b78db78124a22c136
Grimmer Kang <grimmer0125@gmail.com>
2016-09-20 23:01:41 +0800

    automatically execute the start/stop scripts.

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// import {spawnSync} from 'child_process';
+// import {spawn} from 'child_process';
+// require('child_process').spawn()
+var child_process =  require('child_process');
+child_process.spawn('sh', ["start.sh"], {
+ // detached: true, <-????
+ // stdio: [ 'ignore', out, err ]
+});
+
 var app = require('app');
 var BrowserWindow = require('browser-window');
 
@@ -29,3 +38,11 @@ app.on('ready', function() {
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
 });
+
+app.on('before-quit', function() {
+  console.log("before-quit");
+  child_process.spawn('sh', ["stop.sh"], {
+   // detached: true, <-????
+   // stdio: [ 'ignore', out, err ]
+  });
+});//willQuitApp = true);
