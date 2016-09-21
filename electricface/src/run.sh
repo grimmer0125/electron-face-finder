e43950f943c9c415564c663abc046190d153eb9b
Grimmer Kang <grimmer0125@gmail.com>
2016-09-22 01:45:40 +0800

    Move electron-server.py codes to another repo.

--- electricface/src/run.sh
+++
@@ -1,6 +0,0 @@
-cd /root/openface/demos/web/
-cp /electricface/src/electron-start.sh .
-cp /electricface/src/electron-server.py .
-chmod 777 ./electron-start.sh
-chmod 777 ./electron-server.py
-./electron-start.sh
