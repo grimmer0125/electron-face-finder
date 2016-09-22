0163448884c9f73be05bf476f30bd95db3ec7c04
Grimmer Kang <grimmer0125@gmail.com>
2016-09-22 15:01:55 +0800

    Fix the problem that launching scrips would not work when running as a packaged electron app. use /usr/local/bin/docker instead of docker

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 
 console.log("start");
 
+// console.log("sh:", process.env.SHELL);
+
+var shellPath = process.env.SHELL;
 var projectPath = __dirname;
+// console.log("project:", projectPath);
+// console.log("project2:", path.resolve(projectPath));
+
 var child_process =  require('child_process');
 // var needIntall = false;
 
@@ -30,19 +36,49 @@ var child_process =  require('child_process');
 
 var runProductMode = false;
 
+// the key point is the absolute path of docker,
+// ref: https://discuss.atom.io/t/every-spawn-task-throws-enoent-when-electron-app-packaged-for-distribution/18692/2
+
+// var docker = "/usr/local/bin/docker";
+// var args = ["run", "-d", "-p", "9000:9000", "-t", "-i", "--name", "faceserver", "grimmer0125/electricface"];
+// child_process.spawn(docker, args, {});
+
+
 if (process.env.NODE_ENV =="debug"){
   console.log("use debug");
   // child_process.spawn('sh', ["start-dev.sh"], {});
+
 } else if (process.env.NODE_ENV =="dev"){
 
   var startScriptPath = projectPath+"/scripts/docker-start-dev.sh";
   console.log("start dev script:", startScriptPath);
-  child_process.spawn('sh', [startScriptPath], {});
+
+  child_process.spawn(shellPath, ['-i', '-c', startScriptPath], {});
+
+  // child_process.spawn(process.env.SHELL, ['-c', 'cd ' + projectPath + ' && scripts/docker-start-dev.sh'])
+
+  // child_process.execFileSync(process.env.SHELL, ['-i', '-c', 'launchctl setenv PATH "$PATH"'])
+  // child_process.spawn(startScriptPath, [], {});
+
 } else {
   runProductMode = true;
-  var startScriptPath = projectPath+"/scripts/docker-start.sh";
-  console.log("start script:", startScriptPath);
-  child_process.spawn('sh', [startScriptPath], {});
+  // var startScriptPath = projectPath+"/scripts/docker-start.sh";
+  console.log("start production script");
+
+  // 1
+  // child_process.spawn(shellPath, ['-i','-c',startScriptPath], {});
+
+  // 2
+  // child_process.spawn(process.env.SHELL, ['-c', 'cd ' + projectPath + ' && scripts/docker-start.sh'])
+
+  // 3
+  // child_process.execFileSync(process.env.SHELL, ['-i', '-c', 'launchctl setenv PATH "$PATH"'])
+  // child_process.spawn(startScriptPath, [], {});
+
+  // 4
+  var docker = "/usr/local/bin/docker";
+  var args = ["run", "-d", "-p", "9000:9000", "-t", "-i", "--name", "faceserver", "grimmer0125/electricface"];
+  child_process.spawn(docker, args, {});
 }
 
 var app = require('app');
@@ -70,16 +106,21 @@ app.on('ready', function() {
 
     mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 
-    // Open the DevTools.
     if (runProductMode == false){
+      // Open the DevTools.
       mainWindow.webContents.openDevTools();
     }
 });
 
 app.on('before-quit', function() {
   console.log("before-quit");
-  child_process.spawn('sh', [projectPath+"/scripts/docker-stop.sh"], {
+
+  var docker = "/usr/local/bin/docker";
+  var args = ["rm", "-f", "faceserver"];
+  child_process.spawn(docker, args, {});
+
+  // child_process.spawn('sh', [projectPath+"/scripts/docker-stop.sh"], {
    // detached: true, <-????
    // stdio: [ 'ignore', out, err ]
-  });
+  // });
 });//willQuitApp = true);
--- scripts/docker-start.sh
+++
@@ -1,2 +0,0 @@
-# docker rm -f faceserver
-docker run -d -p 9000:9000 -t -i --name faceserver grimmer0125/electricface
