a24761da2b1314140cc35b895e20c4b28ca32861
Grimmer Kang <grimmer0125@gmail.com>
2016-10-04 00:17:56 +0800

    Add the handle on special path on win platform.

--- app/js/file-system.js
+++ app/js/file-system.js
@@ -3,25 +3,12 @@ var utilities = require('./utilities');
 var _ = require('lodash');
 
 module.exports = {
-	// getDirectoryImageFiles: function(dir) {
-	// 	var files = fs.readdirSync(dir);
-	// 	var fullFilePaths = _.map(files, function(fileName) {
-	// 		return path.join(dir, fileName);
-	// 	});
-	//
-	// 	var imageFiles = _.filter(fullFilePaths, utilities.isSupportedImageFile);
-	//
-	// 	return imageFiles;
-	// },
 	getAllImageFiles: function(dir) {
-
-		// console.log("process type3:", process.type)
 
 		console.log('select dir:', dir);
 		var imageFiles = [];
 
 		getImages(dir, imageFiles);
-		// console.log('all leng:', imageFiles);
 
 		return imageFiles;
 	}
